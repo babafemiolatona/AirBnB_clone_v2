04fe89baacb87daab890b1038a36c04afb67ff20
babafemiolatona <femiolatona@gmail.com>
2023-10-20 17:23:59 +0100

    Update

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
-"""Starts a flask web application"""
+"""Starts a web app"""
 from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
     return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
 
 
 if __name__ == "__main__":
