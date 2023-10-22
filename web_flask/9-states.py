6ab681a289038315ab401bb13272912ee9f9d662
babafemiolatona <femiolatona@gmail.com>
2023-10-23 00:49:23 +0100

    Update

---
+++ web_flask/9-states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Starts a Flask Web Application"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.city import City
+
+
+app = Flask(__name__)
+
+
+@app.route('/states', strict_slashes=False)
+def states():
+    """Displays an HTML page with a list of all State objects in DBStorage"""
+    states = storage.all(State)
+    return render_template('9-states.html', states=states)
+
+
+@app.route("/states/<id>", strict_slashes=False)
+def states_id(id):
+    """Displays an HTML page with info about <id>, if it exists."""
+    for state in storage.all("State").values():
+        if state.id == id:
+            return render_template("9-states.html", state=state)
+    return render_template("9-states.html")
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Removes the current SQLAlchemy Session"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
