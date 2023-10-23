92c4ebbe444070b7d651fb0c7ed901bb1adf7ce2
babafemiolatona <femiolatona@gmail.com>
2023-10-23 01:00:03 +0100

    Refactored code

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Starts a Flask Web Application"""
+"""Starts a Flash Web Application"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
@@ -9,27 +9,25 @@ from models.city import City
 app = Flask(__name__)
 
 
-@app.route('/states', strict_slashes=False)
-def states():
-    """Displays an HTML page with a list of all State objects in DBStorage"""
-    states = storage.all(State)
-    return render_template('9-states.html', states=states)
-
-
-@app.route("/states/<id>", strict_slashes=False)
-def states_id(id):
-    """Displays an HTML page with info about <id>, if it exists."""
-    for state in storage.all("State").values():
-        if state.id == id:
-            return render_template("9-states.html", state=state)
-    return render_template("9-states.html")
-
-
 @app.teardown_appcontext
-def teardown_db(exception):
-    """Removes the current SQLAlchemy Session"""
+def teardown_context(exception):
+    """Close storage when teardown"""
     storage.close()
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+@app.route("/states", strict_slashes=False)
+@app.route("/states/<id>", strict_slashes=False)
+def state_id(id=None):
+    """get state by id
+
+    Returns:
+        template: state template
+    """
+    states = storage.all(State)
+    cities = storage.all(City)
+    return render_template("9-states.html",
+                           states=states, cities=cities, id=id)
+
+
+if __name__ == "__main__":
+    app.run('0.0.0.0', port=5000)
